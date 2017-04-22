c063068d6625cbd2032a6e5bfc5eee220051758a
supernoir <kawohl.bentodesign@gmail.com>
2017-04-23 00:57:53 +0200

    sets up user rendering in frontend

---
+++ components/UserList.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+class UserList extends React.Component {
+    constructor(props){
+       super(props);
+       this.props = {
+           id: this.props.id,
+           name: this.props.name,
+           location: this.props.location,
+           gender: this.props.gender,
+           interests: this.props.interests,
+       }
+    }
+
+    render(){
+        return (
+            <tr>
+                <td>{this.props.name}</td>
+                <td>{this.props.location}</td>
+                <td>{this.props.gender}</td>
+                <td>{this.props.interests}</td>
+            </tr>
+              )
+    }
+}
+
+export default UserList;
--- index.js
+++ index.js
@@ -3,6 +3,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import UserList from './components/UserList'
+
+const users = [
+    {
+      "name": "Jael Bat Meir",
+      "location": "Paris, France",
+      "gender": "Female",
+      "interests": "Mishna Yomit"
+    },
+    {
+      "name": "Miriam bat Avraham",
+      "location": "Berlin, Germany",
+      "gender": "Female",
+      "interests": "Kuzari"
+    },
+    {
+      "name": "Rachel Levy",
+      "location": "New York City, USA",
+      "gender": "Female",
+      "interests": "Mishna Yomit"
+    },
+    {
+      "name": "Ayelet Shahar",
+      "location": "Jerusalem, Israel",
+      "gender": "Female",
+      "interests": "Kuzari"
+    },
+]
+
 class App extends React.Component {
     constructor(){
         super()
@@ -11,7 +40,49 @@ class App extends React.Component {
         return (
             <div>
                 <h1>Chavruta</h1>
+                <Table />
             </div>
+        )
+    }
+}
+
+let Table = () => {
+    return (
+        <table>
+            <TableHead />
+            <TableRows />
+        </table>
+    )
+}
+
+let TableHead = () => {
+    return (
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Location</th>
+                <th>Gender</th>
+                <th>Interests</th>
+            </tr>
+        </thead>
+    )
+}
+
+class TableRows extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            users: users
+        }
+    }
+
+    render() {
+        return (
+            <tbody>
+            {this.state.users.map((user) => (
+                <UserList key={user.id} name={user.name} location={user.location} gender={user.gender} interests={user.interests} />
+            ))}
+            </tbody>
         )
     }
 }
